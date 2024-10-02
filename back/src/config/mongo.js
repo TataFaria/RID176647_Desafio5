cca96982a82e52602850a25ac2485a3f30f35ec2
Thaylla <167567044+TataFaria@users.noreply.github.com>
2024-10-02 16:11:06 -0300

    Atualizando a pasta do banco de dados

--- back/src/config/mongo.js
+++ back/src/config/mongo.js
@@ -7,7 +7,7 @@ const connection = () => {
         return conn
     }
     
-    conn = mongoose.connect('mongodb://127.0.0.1:27017/local')
+    conn = mongoose.connect('mongodb://localhost:27017')
 }
 
 module.exports = connection
